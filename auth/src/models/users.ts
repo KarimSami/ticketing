09a66b20412df017746d247efd5fec80cd88032a
Karim Sami <karimsami047@gmail.com>
2023-07-25 11:28:05 +0300

    Build the users model

---
+++ auth/src/models/users.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+// An interface that describes the properties
+// that are required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+//An interfce that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+export const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User as default };
