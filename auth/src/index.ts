65ffa99ca6be9c7efb7523274c9c373a1b7d4ed1
Karim Sami <karimsami047@gmail.com>
2023-07-25 10:54:39 +0300

    Connect the app to the database

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-
+import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -23,6 +23,16 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.error(e);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
