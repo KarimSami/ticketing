66b54fb65a9691fe018ce598a45ffa75aaf1d217
Karim Sami <karimsami047@gmail.com>
2023-07-25 11:57:15 +0300

    Add the initial logic to signup a user

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/users";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -14,16 +15,22 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.body;
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
 
-    res.send({});
+    try {
+      const user = User.build({ email, password });
+      await user.save();
+      res.status(201).send(user);
+    } catch (e: any) {
+      if (e.code === 11000) {
+        throw new BadRequestError("Email in use");
+      }
+    }
   }
 );
 
